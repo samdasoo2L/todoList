800222e72011d9097d61be040f968a549b700403
samdasoo2L <alstjr8125@naver.com>
2022-03-12 19:14:26 +0900

    1

---
+++ app.js
@@ -0,0 +1,8 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit() {
+  const username = loginInput.value;
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
